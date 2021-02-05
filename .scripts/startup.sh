53e9d66f9697bbcd25d515905056d0ae7ce51b50
Numb4r <yuridimitre@protonmail.com>
2021-02-05 10:23:05 -0300

    Adicionado wallpaper video

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -1,5 +1,6 @@
 picom -bC --config ~/.config/picom/picom.conf
 feh --randomize --bg-fill ~/Wallpapers
+~/.scripts/xwinwrap.sh &
 #dwmstatus 2>&1 >/dev/null &
 export PATH="$PATH:~/git/flutter/bin"
 #pcmanfm --desktop &
